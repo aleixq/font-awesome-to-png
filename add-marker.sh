0d3c403841a28ceb53bb3d32891aa9e0099cec18
Aleix Quintana Alsius (kinta) <kinta@communia.org>
2014-10-14 23:31:50 +0200

    little icons

--- add-marker.sh
+++ add-marker.sh
@@ -11,14 +11,13 @@ hicon=`identify -format "%h" $someIcon`
 wback=`identify -format "%w" markers-matte.png`
 hback=`identify -format "%h" markers-matte.png`
 
-xoff=`expr $wback / 2 - $( expr $wicon / 2 ) `
-echo $wback / 2 -  $wicon / 2
+xoff=`echo "scale=2 ; $wback  / 2 - $( echo "scale=2 ; $wicon  / 2 - 1" | bc ) " | bc `
 mkdir -p  $target
 
 if [ -d "$source" ]
 then
 	for file in `find out/ -name '*.png'` ; do
-	 composite -geometry +$xoff+12  "$file" "$background" "${target}/`basename $file .png`.png"
+	 composite -geometry +$xoff+6  "$file" "$background" "${target}/`basename $file .png`.png"
 	done
 else
 	echo "run getAndbuild.sh previously"
--- getAndBuild.sh
+++ getAndBuild.sh
@@ -1,4 +1,4 @@
 mkdir -p out
 wget -O - https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/fontawesome-webfont.ttf  > out/fontawesome-webfont.ttf
 cd out
-python3 ../font-awesome-to-png.py ALL --size 16
+python3 ../font-awesome-to-png.py ALL --size 24
